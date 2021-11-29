af0c4f9949fef73039431ba4f8696c99c172a736
Tomasz Ossowski <ossseek@gmail.com>
2021-11-29 12:52:54 +0100

    Added loop 'for' in class Program

--- Program.cs
+++ Program.cs
@@ -6,7 +6,9 @@ namespace first
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            for(int i = 0; i<5; i++){
+                Console.WriteLine(i);
+            }
         }
     }
 }
